906d2d9f90db4aac2cf8837f7ee82a281d3f960e
mihaelpucekovic <mihaelpucekovic@gmail.com>
2020-09-25 10:36:29 +0200

    Initial Commit

---
+++ Discover/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Discover
+//
+//  Created by Mihael Puceković on 25/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
